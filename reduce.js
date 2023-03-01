a18ad68102e06c75b3e4d416bde7367867126465
Mohon Saha <96807076+MohonSaha@users.noreply.github.com>
2023-03-01 09:13:40 +0600

    Update reduce.js
    Update defination

--- reduce.js
+++ reduce.js
@@ -1,6 +1,12 @@
 // Reduce method:
 
 /*
+reduce(accumulatorFunction, initial value);
+Accumulator Function use two parameters.
+
+*/
+
+/*
 const numbers = [1, 2, 3, 4, 5];
 const total = numbers.reduce((previous, current) => previous + current, 0);
 console.log(total);
